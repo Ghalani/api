d7c4f43d6fcb9724bc4a3d33547f8f768d2d0e90
Amanze Ogbonna <darlingtonamz@gmail.com>
2016-04-19 16:51:14 +0000

    Commented in route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'root#index'
+  # for any other route
   match '*a', :to => 'errors#routing', via: [:get, :post, :put, :patch, :delete, :options, :head]
 end
