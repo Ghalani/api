e8eb5cf89ef6ad628a647b6760f5a6fb7e765850
Amanze Ogbonna <darlingtonamz@gmail.com>
2016-04-20 10:15:57 +0000

    Deleted everything

--- app/controllers/errors_controller.rb
+++
@@ -1,5 +0,0 @@
-class ErrorsController < ApplicationController
-  def routing
-    render json: {"error":"No route matches [#{request.method}] #{request.path}"}, status: 404
-  end
-end
--- config/routes.rb
+++
@@ -1,4 +0,0 @@
-Rails.application.routes.draw do
-  root 'root#index'
-  match '*a', :to => 'errors#routing', via: [:get, :post, :put, :patch, :delete, :options, :head]
-end
